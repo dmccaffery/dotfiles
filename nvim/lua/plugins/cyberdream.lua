c7add620a92a8e322a5ae03124ff35d9837bd523
Deavon McCaffery <dmccaffery@users.noreply.github.com>
2025-06-20 12:54:20 +0100

    feat(nvim): use cyberdream theme in neovim

---
+++ nvim/lua/plugins/cyberdream.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      theme = "auto",
+    },
+  },
+}
