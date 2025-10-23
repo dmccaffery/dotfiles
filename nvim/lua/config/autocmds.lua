7340a8df4a43a262d8a21faa948103ad41045bcb
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2025-10-23 06:06:51 +0100

    feat(nvim): add autocmds to fix some annoying things
    * auto-close certain buffers / dialogs
    * switch between relative (visual/normal) and absolute (insert) line
    numbers
    * disable leader key in some buffers
    
    Signed-off-by: Deavon M. McCaffery <dmccaffery@users.noreply.github.com>

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,11 +2,62 @@
 -- defaults: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 
 autocmd("FileType", {
   pattern = "qf",
   callback = function()
     vim.opt_local.buflisted = false
+  end,
+})
+
+local auto_close_filetype = {
+  "lazy",
+  "mason",
+  "lspinfo",
+  "toggleterm",
+  "null-ls-info",
+  "notify",
+}
+
+autocmd("BufLeave", {
+  group = augroup("lazyvim_auto_close_win", { clear = true }),
+  callback = function(event)
+    local ft = vim.api.nvim_get_option_value("filetype", { buf = event.buf })
+
+    if vim.fn.index(auto_close_filetype, ft) ~= -1 then
+      local winids = vim.fn.win_findbuf(event.buf)
+      for _, win in pairs(winids) do
+        vim.api.nvim_win_close(win, true)
+      end
+    end
+  end,
+})
+
+-- disable leader for some buffers
+autocmd("FileType", {
+  group = augroup("lazyvim_unbind_leader_key", { clear = true }),
+  pattern = {
+    "lazy",
+    "mason",
+    "lspinfo",
+    "toggleterm",
+    "null-ls-info",
+    "neo-tree-popup",
+    "notify",
+    "floaterm",
+  },
+  callback = function(event)
+    vim.keymap.set("n", "<leader>", "<nop>", { buffer = event.buf, desc = "" })
+    vim.keymap.set("n", "<localleader>", "<nop>", { buffer = event.buf, desc = "" })
+  end,
+})
+
+-- disable spell check and numbers in built-in terminals
+autocmd("TermOpen", {
+  callback = function()
+    vim.cmd("setlocal listchars= nonumber norelativenumber")
+    vim.cmd("setlocal nospell")
   end,
 })
 
@@ -16,9 +67,40 @@ autocmd("BufWritePre", {
   command = [[%s/\s\+$//e]],
 })
 
+-- do not continue comments on next lines
+autocmd("BufEnter", {
+  callback = function()
+    vim.cmd("set formatoptions-=cro")
+    vim.cmd("setlocal formatoptions-=cro")
+  end,
+})
+
 autocmd("VimEnter", {
   callback = function()
     require("persistence").load()
   end,
   nested = true,
+})
+
+-- toggle between relative and absolute line numbers
+local numbertoggle = augroup("numbertoggle", { clear = true })
+autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+  pattern = "*",
+  group = numbertoggle,
+  callback = function()
+    if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+
+autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+  pattern = "*",
+  group = numbertoggle,
+  callback = function()
+    if vim.o.nu then
+      vim.opt.relativenumber = false
+      vim.cmd.redraw()
+    end
+  end,
 })
