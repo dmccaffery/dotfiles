db54610f4716eb3fc508866f720e342a5b204d95
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2025-10-23 06:09:57 +0100

    feat(lazygit): use delta for git diffs
    Signed-off-by: Deavon M. McCaffery <dmccaffery@users.noreply.github.com>

--- setup/stow.sh
+++ setup/stow.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . "${SCRIPT_DIR}/printing.sh"
 
+INSTALL_DIR="${INSTALL_DIR:-${SCRIPT_DIR}/..}"
+
 if ! command -v stow 1>/dev/null 2>&1; then
 	warn "stow could not be found; did you forget to install brews?"
 else
