f698fa1f84156f6071d795918079d2b9f2e0462e
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2025-10-23 21:34:55 +0100

    fix(install): use xdg in shell setup
    Signed-off-by: Deavon M. McCaffery <dmccaffery@users.noreply.github.com>

--- setup/shell.sh
+++ setup/shell.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 SETUP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . "${SETUP_DIR}/printing.sh"
 
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-${HOME}/.config}
+
 ZSH_BIN="$(command -v zsh)"
 
 if [ "${ZSH_BIN:-}" == "${SHELL:-}" ]; then
@@ -15,15 +17,15 @@ else
 	chsh -s "${ZSH_BIN}"
 fi
 
-ZSHRC_CONFIG="${HOME}/.config/zshrc/.zshrc"
+ZSHRC_CONFIG="${XDG_CONFIG_HOME}/zshrc/.zshrc"
 
 if [ -f "${ZSHRC_CONFIG:-}" ]; then
 	info "linking zshrc to config"
-	ln -Ffs "${HOME}/.config/zshrc/.zshrc" "${HOME}/.zshrc"
+	ln -Ffs "${XDG_CONFIG_HOME}/zshrc/.zshrc" "${HOME}/.zshrc"
 else
 	warning "zshrc config is not present and cannot be symlinked; did you forget to
 	stow?"
 fi
 
-# hush the last login prompt in ttty
+# hush the last login prompt in tty
 touch "${HOME}/.hushlogin"
