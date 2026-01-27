6ce408ad85155281e1368c7212ab9ee5a188e4a5
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2026-01-27 13:51:11 +0000

    feat(nvim): update all dependencies

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -45,7 +45,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.xml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     -- plugins
     { import = "plugins" },
