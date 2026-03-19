41e1e76952f8c523eff63ece7ce6ddf68b5bda8b
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2026-03-19 23:27:27 +0000

    fix(setup): protect against word splitting

--- setup/brews.sh
+++ setup/brews.sh
@@ -41,7 +41,7 @@ brew reinstall stow 1> /dev/null 2>&1
 # setup buildx
 if command -v docker-buildx 1> /dev/null 2>&1; then
 	mkdir -p "${HOME}/.docker/cli-plugins"
-	ln -fns $(command -v docker-buildx 2> /dev/null) "${HOME}/.docker/cli-plugins"
+	ln -fns "$(command -v docker-buildx 2> /dev/null)" "${HOME}/.docker/cli-plugins"
 fi
 
 # cleanup services
