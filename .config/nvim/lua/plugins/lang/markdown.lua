c8305e9ceb97fbd653687f1f39929398029fa867
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2026-04-17 12:45:55 +0100

    chore(nvim): use prettier for markdown again
    Signed-off-by: Deavon M. McCaffery <dmccaffery@users.noreply.github.com>

--- .config/nvim/lua/plugins/lang/markdown.lua
+++ .config/nvim/lua/plugins/lang/markdown.lua
@@ -1,11 +1,6 @@
 return {
   {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        ["markdown"] = { "markdownlint-cli2", "markdown-toc" },
-        ["markdown.mdx"] = { "markdownlint-cli2", "markdown-toc" },
-      },
-    },
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "mmdc" } },
   },
 }
