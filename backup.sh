f27e0f601e945d51218b7ec6327b1169aba0f7e6
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2026-05-25 20:56:28 +0100

    feat(backup): improve backup script
    * allow user-defined name for backup
    * only backup things that are necessary (not all of .config and .local)
    * improve reliability
    
    Signed-off-by: Deavon M. McCaffery <dmccaffery@users.noreply.github.com>

--- backup.sh
+++ backup.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env sh
 set -eu
 
-now=$(date +'%Y-%M-%d-%H%M%S')
-backup_path="backups/${now}"
+BACKUP_DIR=$(dirname "$(readlink -f -- "$0")")
+. "${BACKUP_DIR}/setup/printing.sh"
+
+name="${1:-$(date +'%Y-%M-%d-%H%M%S')}"
+backup_path="${BACKUP_DIR}/backups/${name}"
 
 mkdir -p "${backup_path}" 1> /dev/null 2>&1 || true
 
@@ -15,15 +18,62 @@ __backup() {
 
 	path="${HOME}/${name}"
 
-	if [ -d "${path:-}" ] && [ ! -h "${path:-}" ]; then
-		printf 'backing up %s to %s\n' "${name}" "${backup_path}"
-		mv "${path}" "${backup_path}"
+	if [ ! -e "${path:-}" ]; then
+		info "${path} does not exist, skipping..."
+		return 0
 	fi
+
+	# do not backup if its already a symlink
+	if [ -h "${path:-}" ]; then
+		info "${path} is already stowed, skipping..."
+		return 0
+	fi
+
+	# create the directory in the backup and mv just the file
+	if [ -f "${path:-}" ]; then
+		dir="${backup_path}/$(dirname "${name}")"
+		warn "backing up file ${name} to ${dir}..."
+
+		mkdir -p "${dir}"
+		mv "${path}" "${dir}"
+
+		return 0
+	fi
+
+	if [ -d "${path:-}" ]; then
+		warn "backing up directory ${name} to ${backup_path}..."
+		mv "${path}" "${backup_path}"
+
+		return 0
+	fi
+
+	error "unknown path ${path:-} or directory entry type"
+	return 1
 }
 
-__backup .claude
-__backup .local
-__backup .ssh
-__backup .terminfo
-__backup .config
+__backup .claude/settings.json
+__backup .claude/themes
+
+(
+	cd "${BACKUP_DIR}"
+	find .config/ -mindepth 1 -maxdepth 1 |
+		while read -r config; do
+			if [ -z "${config:-}" ]; then
+				break
+			fi
+
+			__backup "${config}"
+		done
+)
+
+__backup .local/share/scripts
+__backup .local/share/wallpapers
+
+__backup .ssh/rc
+
+__backup .terminfo/67/ghostty
+__backup .terminfo/78/xterm-ghostty
+
+__backup Library/LaunchAgents/org.homebrew.ssh-agent.plist
+
 __backup .zshrc
