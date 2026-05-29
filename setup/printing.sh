798cf701a165f8962d5bb41835e1c0a66ff2e749
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2026-05-29 22:36:50 +0100

    fix(scripts): send diagnostic output to stderr
    warn/error helpers printed to stdout, which corrupts the output of any caller that captures a script's stdout. Route warnings and errors to stderr so only intended values land on stdout.
    
    setup/printing.sh additionally reorders the helpers into info/warn/success/error, keeping info/success on stdout.
    
    Signed-off-by: Deavon M. McCaffery <dmccaffery@users.noreply.github.com>

--- setup/printing.sh
+++ setup/printing.sh
@@ -4,9 +4,14 @@ set -eu
 
 default_color=$(tput sgr 0)
 
-red="$(tput setaf 1)"
-error() {
-	printf "%s==> %s%s\n" "$red" "$1" "$default_color"
+blue="$(tput setaf 4)"
+info() {
+	printf "%s==> %s%s\n" "$blue" "$1" "$default_color"
+}
+
+yellow="$(tput setaf 3)"
+warn() {
+	printf "%s==> %s%s\n" "$yellow" "$1" "$default_color" >&2
 }
 
 green="$(tput setaf 2)"
@@ -14,12 +19,7 @@ success() {
 	printf "%s==> %s%s\n" "$green" "$1" "$default_color"
 }
 
-yellow="$(tput setaf 3)"
-warn() {
-	printf "%s==> %s%s\n" "$yellow" "$1" "$default_color"
-}
-
-blue="$(tput setaf 4)"
-info() {
-	printf "%s==> %s%s\n" "$blue" "$1" "$default_color"
+red="$(tput setaf 1)"
+error() {
+	printf "%s==> %s%s\n" "$red" "$1" "$default_color" >&2
 }
