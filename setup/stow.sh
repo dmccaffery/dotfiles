8d94836ae372d84a6dceaa8c9a114c6267d4b353
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2026-06-07 00:02:01 +0100

    fix(stow): stow specific paths to avoid maintenance of stowrc
    Signed-off-by: Deavon M. McCaffery <dmccaffery@users.noreply.github.com>

--- setup/stow.sh
+++ setup/stow.sh
@@ -7,15 +7,25 @@ SETUP_DIR=$(dirname "$(readlink -f -- "$0")")
 
 info "stowing config..."
 
-INSTALL_DIR="${INSTALL_DIR:-${SETUP_DIR}/..}"
+INSTALL_DIR="$(realpath "${INSTALL_DIR:-${SETUP_DIR}/..}")"
 
 if ! command -v stow 1> /dev/null 2>&1; then
 	warn "stow could not be found; did you forget to install brews?"
 else
 	info "linking config symlinks to repository"
 	mkdir -p "${HOME}/.claude"
-	mkdir -p "${HOME}/.config"
+	mkdir -p "${HOME}/.config/codex"
+	mkdir -p "${HOME}/.config/opencode"
+	mkdir -p "${HOME}/.config/zsh"
+	mkdir -p "${HOME}/.local/share/scripts"
+	mkdir -p "${HOME}/.local/share/wallpapers"
 	mkdir -p "${HOME}/.ssh"
 
-	stow --dir="${INSTALL_DIR}" .
+	stow --dir="${INSTALL_DIR}/.config" --target="${HOME}/.config" .
+	stow --dir="${INSTALL_DIR}/.local" --target="${HOME}/.local" .
+	stow --dir="${INSTALL_DIR}/.terminfo" --target="${HOME}/.terminfo" .
+	stow --dir="${INSTALL_DIR}/.ssh" --target="${HOME}/.ssh" .
+	stow --dir="${INSTALL_DIR}/Library" --target="${HOME}/Library" .
+
+	ln -Ffs "${INSTALL_DIR}/.zshenv" "${HOME}/.zshenv"
 fi
